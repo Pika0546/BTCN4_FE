75fe3549be8e5060361a9e9c4007522e54b41078
khoale <kthedestroy@gmail.com>
2022-10-24 21:46:46 +0700

    remove env

--- src/services/account.js
+++ src/services/account.js
@@ -1,10 +1,7 @@
+import { API_URL } from "../config";
 import { makeRequest } from "../lib/api"
 import { HTTPMethod } from "../lib/common"
 
-const API = process.env.REACT_APP_API_HOST;
-
-
 export const getAccountList = async () => {
-    console.log(`${process.env.API_HOST}/account`);
-    return makeRequest(HTTPMethod.GET, `${API}/account`)
+    return makeRequest(HTTPMethod.GET, `${API_URL}/account`)
 }
--- src/services/signup.js
+++ src/services/signup.js
@@ -1,11 +1,11 @@
+import { API_URL } from "../config"
 import { makeRequest } from "../lib/api"
 import { HTTPMethod } from "../lib/common"
 
-const API = process.env.REACT_APP_API_HOST;
 
 export const createAccount = async ({
     username,
     password
 }) => {
-    return makeRequest(HTTPMethod.POST, `${API}/account`, {username, password})
+    return makeRequest(HTTPMethod.POST, `${API_URL}/account`, {username, password})
 }
